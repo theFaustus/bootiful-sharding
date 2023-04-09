c6954e0d0d71695ce3aa1115ce01bf354a77e258
Ion Pascari <Ion.Pascari@orange.com>
2023-04-10 00:14:00 +0300

    feat(sharding): add shardingsphere

--- src/main/kotlin/inc/evil/bootifulsharding/BootifulShardingApplication.kt
+++ src/main/kotlin/inc/evil/bootifulsharding/BootifulShardingApplication.kt
@@ -2,6 +2,8 @@ package inc.evil.bootifulsharding
 
 import inc.evil.bootifulsharding.model.Review
 import inc.evil.bootifulsharding.repo.ReviewRepository
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
@@ -15,13 +17,34 @@ import org.springframework.scheduling.annotation.EnableScheduling
 @EnableConfigurationProperties
 @ConfigurationPropertiesScan
 class BootifulShardingApplication {
-
+    companion object {
+        private val log: Logger = LoggerFactory.getLogger(this::class.java)
+    }
     @Bean
     fun init(reviewRepository: ReviewRepository) = CommandLineRunner {
         run {
-            (0..100).forEach { i ->
-                reviewRepository.save(Review(text = "Review number $i", author = "Mike Scott", courseId = i))
-            }
+//            (0..10000).forEach { i ->
+//                reviewRepository.save(Review(text = "Review number $i", author = "Mike Scott", courseId = i))
+//            }
+            reviewRepository.findAllById(listOf(1152)).forEach { log.info(it.toString()) }
+            reviewRepository.findAllById(listOf(1152)).forEach { log.info(it.toString()) }
+            reviewRepository.findAllById(listOf(1152)).forEach { log.info(it.toString()) }
+            reviewRepository.findAllById(listOf(1152)).forEach { log.info(it.toString()) }
+            reviewRepository.findAllById(listOf(1152)).forEach { log.info(it.toString()) }
+            reviewRepository.findAllById(listOf(1152)).forEach { log.info(it.toString()) }
+            reviewRepository.findAllById(listOf(1152)).forEach { log.info(it.toString()) }
+            reviewRepository.findAllById(listOf(1152)).forEach { log.info(it.toString()) }
+            reviewRepository.findAllById(listOf(1152)).forEach { log.info(it.toString()) }
+            reviewRepository.findAllById(listOf(1152)).forEach { log.info(it.toString()) }
+            reviewRepository.findAllById(listOf(1152)).forEach { log.info(it.toString()) }
+            reviewRepository.findAllById(listOf(1152)).forEach { log.info(it.toString()) }
+            reviewRepository.findAllById(listOf(1152)).forEach { log.info(it.toString()) }
+            reviewRepository.findAllById(listOf(1152)).forEach { log.info(it.toString()) }
+            reviewRepository.findAllById(listOf(1152)).forEach { log.info(it.toString()) }
+            reviewRepository.findAllById(listOf(1152)).forEach { log.info(it.toString()) }
+            reviewRepository.findAllById(listOf(1152)).forEach { log.info(it.toString()) }
+            reviewRepository.findAllById(listOf(1152)).forEach { log.info(it.toString()) }
+            reviewRepository.findAllById(listOf(1152)).forEach { log.info(it.toString()) }
         }
     }
 }
